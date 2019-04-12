203f48c4bf3271aeb952d46a13fe4d02246c9791
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-12 10:40:30 +0100

    initial commit

---
+++ automate_form.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from datetime import *
+from time import gmtime, strftime
+import random as random
+from random import randrange
+
+
+def get_time():
+    global d
+    global s_ran_hour
+    global s_ran_min
+    global f_ran_min
+    global f_ran_hour
+    global t_time
+
+    d = date.today()
+    s_ran_hour = random.randint(8, 9)
+    s_ran_min = randrange(60)
+    f_ran_hour = random.randint(21, 23)
+    f_ran_min = randrange(60)
+    t_time = '{}:{}'.format(f_ran_hour, f_ran_min)
+    _timer = datetime(d.year, d.month, d.day, f_ran_hour, f_ran_min)
+
+
+def initialization():
+    global driver
+
+    driver = webdriver.Chrome(executable_path=r"C:\Program Files\chrome driver\chromedriver.exe")
+    driver.get('https://goo.gl/forms/EW8UxiE56ACtYrS73')
+
+
+def attendance():
+    initialization()
+    xpath_id = '//*[@id="mG61Hd"]/div/div[2]/div[2]/div[1]/div/div[2]/div/div[1]/div/div[1]/input'
+    student_id = driver.find_element_by_xpath(xpath_id)
+    first_name = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input')
+    surname = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[3]/div/div[2]/div/div[1]/div/div[1]/input')
+    office_no = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[4]/div/div[2]/div/div[1]/div/div[1]/input')
+    date_ = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[5]/div/div[2]/div/div[2]/div[1]/div/div[1]/input')
+    time_in_h = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[6]/div/div[2]/div[1]/div[2]/div[1]/div/div[1]/input')
+    time_in_m = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[6]/div/div[2]/div[3]/div/div[1]/div/div[1]/input')
+    #time_in_ap = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[6]/div/div[2]/div[4]/div[1]/div[1]/div[1]/content')
+    time_out_h = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[7]/div/div[2]/div[1]/div[2]/div[1]/div/div[1]/input')
+    time_out_m = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[7]/div/div[2]/div[3]/div/div[1]/div/div[1]/input')
+    #time_out_ap = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[7]/div/div[2]/div[4]/div[1]/div[1]/div[1]')
+    note = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[2]/div[8]/div/div[2]/div/div[1]/div/div[1]/input')
+    form = driver.find_element_by_xpath(xpath='//*[@id="mG61Hd"]/div/div[2]/div[3]/div[1]/div/div/content/span')
+
+    student_id.send_keys('3331605')
+    first_name.send_keys('Emeka')
+    surname.send_keys('Ugwuanyi')
+    office_no.send_keys('FW210')
+    date_.send_keys('{}/{}/{}'.format(d.day, d.month, d.year))
+    #date_.send_keys('04/04/2019')
+    time_in_m.send_keys(s_ran_min)
+    time_in_h.send_keys(s_ran_hour)
+    time_out_h.send_keys(f_ran_hour)
+    time_out_m.send_keys(f_ran_min)
+    note.send_keys("Emeka's attendance")
+    form.click()
+
+
+def main():
+    while True:
+        get_time()
+        time_now = datetime.now().strftime("%H:%M")
+        if time_now == t_time:
+            attendance()
+
+
---
+++ automate_work_form.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from datetime import *
+import calendar
+
+
+__author__ = 'Emmanuel'
+
+d = date.today()
+
+
+def _day():
+    return calendar.day_name[d.weekday()]
+
+
+def initialization():
+    global driver
+
+    driver = webdriver.Chrome(executable_path=r"C:\Program Files\chrome driver\chromedriver.exe")
+    driver.get('https://mypayments.lsbu.ac.uk/vts/')
+    username = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldUserName"]')
+    password = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldPwd"]')
+    login = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_cmdBar"]/div/div/div/ul/li[1]/a/span/span/span/span')
+
+    username.send_keys('ugwuane3')
+    password.send_keys('1coolnigga')
+
+    login.click()
+
+    submit_claims = driver.find_element_by_xpath(xpath='//*[@id="ctl00_VTSMnu"]/ul/li[7]/a/span')
+    submit_claims.click()
+
+
+def francis(day):
+    initialization()
+    select_CMIT = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_lstSess_ctl00__0"]').click()
+    select_OK = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    select_OK.click()
+
+    add_hours = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a').click()
+
+    work_date = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldWorkDte_dateInput"]')
+
+    start_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldStTime"]')
+
+    finish_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldEnTime"]')
+
+    hours_worked = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldActHours"]')
+
+    sick_hours = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldSickHours"]')
+
+    comment = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldComment"]')
+
+    # add_hours_CMIT = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span').click()
+    work_date.send_keys('{}\{}\{}'.format(d.day, d.month, d.year))
+    #work_date.send_keys('{}\{}\{}'.format(28, 3, 2019))
+    if day == 'tuesday':
+        start_time.send_keys('14:00')
+        finish_time.send_keys('16:00')
+        comment.send_keys('Management Concepts and Evaluation Techniques 1')
+    elif day == 'thursday':
+        start_time.send_keys('11:00')
+        finish_time.send_keys('13:00')
+        comment.send_keys('Management Concepts and Evaluation Techniques 2')
+    hours_worked.send_keys('2')
+    sick_hours.send_keys('0')
+    okay = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    okay.click()
+
+
+def lucia():
+    initialization()
+    select_MIP = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_lstSess_ctl00__1"]').click()
+    select_OK = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    select_OK.click()
+
+    add_hours = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a').click()
+
+    work_date = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldWorkDte_dateInput"]')
+
+    start_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldStTime"]')
+
+    finish_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldEnTime"]')
+
+    hours_worked = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldActHours"]')
+
+    sick_hours = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldSickHours"]')
+
+    comment = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldComment"]')
+    work_date.send_keys('{}\{}\{}'.format(d.day, d.month, d.year))
+    #work_date.send_keys('{}\{}\{}'.format(25, 3, 2019))
+    start_time.send_keys('10:00')
+    finish_time.send_keys('13:00')
+    hours_worked.send_keys('3')
+    sick_hours.send_keys('0')
+    comment.send_keys('ICT Project Management in Practice')
+    okay = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    okay.click()
+
+
+def iot():
+    initialization()
+    select_iot = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_lstSess_ctl00__2"]').click()
+    select_OK = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    select_OK.click()
+
+    add_hours = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a').click()
+
+    work_date = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldWorkDte_dateInput"]')
+
+    start_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldStTime"]')
+
+    finish_time = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldEnTime"]')
+
+    hours_worked = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldActHours"]')
+
+    sick_hours = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldSickHours"]')
+
+    comment = driver.find_element_by_xpath(xpath='//*[@id="ctl00_ContentPlaceHolder1_fldComment"]')
+    work_date.send_keys('{}\{}\{}'.format(d.day, d.month, d.year))
+    #work_date.send_keys('{}\{}\{}'.format(28, 3, 2019))
+    start_time.send_keys('16:00')
+    finish_time.send_keys('18:00')
+    hours_worked.send_keys('2')
+    sick_hours.send_keys('0')
+    comment.send_keys('Embedded Systems and Internet of things')
+    okay = driver.find_element_by_xpath(
+        xpath='//*[@id="ctl00_ContentPlaceHolder1_RadToolBar1"]/div/div/div/ul/li[1]/a/span/span/span')
+    okay.click()
+
+
+#francis(_day().lower())
+#francis('thursday')
+#lucia()
+#iot()
